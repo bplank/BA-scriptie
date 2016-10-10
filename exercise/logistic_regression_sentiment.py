729b485364c64084a0d42f3200f88e4a4616eb55
Barbara Plank <bplank@gmail.com>
2016-10-10 22:19:43 +0200

    exercise

--- exercise/logistic_regression_sentiment.py
+++ exercise/logistic_regression_sentiment.py
@@ -6,8 +6,7 @@ Exercise: sentiment classification with logistic regression
 2) What is the distribution of labels in the data?
 3) Add code to train and evaluate the classifier. What accuracy do you get? What is weird?
 4) How could you improve the representation of the data?
-5) Implement cross-validation.
-
+5) Add confusion matrix output.
 """
 from collections import Counter
 from sklearn.feature_extraction.text import CountVectorizer
@@ -15,14 +14,15 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score, confusion_matrix
 import numpy as np
+import random
 
 def load_sentiment_sentences_and_labels():
     """
     loads the movie review data
     """
     ## Q1: What are the features used? Q4: How could you improve the representation of the data?
-    positive_sentences = [line.strip().split(" ") for line in open("rt-polaritydata/rt-polarity.pos").readlines()]
-    negative_sentences = [line.strip().split(" ") for line in open("rt-polaritydata/rt-polarity.neg").readlines()]
+    positive_sentences = open("rt-polaritydata/rt-polarity.pos").readlines()
+    negative_sentences = open("rt-polaritydata/rt-polarity.neg").readlines()
 
     ## Q2: What is the label distribution?
     positive_labels = [1 for sentence in positive_sentences]
@@ -30,16 +30,24 @@ def load_sentiment_sentences_and_labels():
 
     sentences = np.concatenate([positive_sentences,negative_sentences], axis=0)
     labels = np.concatenate([positive_labels,negative_labels],axis=0)
-    return sentences, labels
+
+    ## make sure we have a label for every data instance
+    assert(len(sentences)==len(labels))
+    data = list(zip(sentences,labels))
+
+    ## return the data (instances + labels)
+    return data
 
 ## read input data
 print("load data..")
-sentences, labels = load_sentiment_sentences_and_labels()
-sentences = [" ".join(sentence) for sentence in sentences]
-
+data = load_sentiment_sentences_and_labels()
+print(len(data))
 # Q: What accuracy do you get when you run the code? What is weird?
 print("split data..")
-split_point = int(0.75*len(sentences))
+split_point = int(0.75*len(data))
+
+sentences = [sentence for sentence, label in data]
+labels = [label for sentence, label in data]
 X_train, X_test = sentences[:split_point], sentences[split_point:]
 y_train, y_test = labels[:split_point], labels[split_point:]
 
@@ -60,19 +68,16 @@ classifier = Pipeline( [('vec', vectorizer),
 ### Q2: add code to train and evaluate your classifier
 print("train model..")
 ## your code here:
-classifier.fit(X_train, y_train)
 
 ##
-print("evaluate model..")
-## your code here:
-y_predicted = classifier.predict(X_test)
+print("predict..")
+## your code here: (instantiate y_predicted)
+
+
 ###
 print("Accuracy:", accuracy_score(y_test, y_predicted))
 
 print("Majority baseline:", accuracy_score(y_test, majority_prediction))
 
-print(confusion_matrix(y_test, y_predicted, labels=[0,1]))
-
-print(y_predicted)
-
-print(vectorizer.vocabulary_.get('the'))
+# you can access the vectorizer (feature to feature number mapping) this way
+# print(vectorizer.vocabulary_.get('the'))
